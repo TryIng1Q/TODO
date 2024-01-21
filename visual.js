658648a103d1a34d33ae058c8da1e7567f26720d
TryIng1Q <kpktdhovord@gmail.com>
2024-01-21 23:49:28 +0600

    add status changes

--- visual.js
+++ visual.js
@@ -24,11 +24,29 @@ const VISUAL_FUNCTIONS = {
 		// Add buttons events
 		todoDoneButton.addEventListener('click', function() {
 			const elementID = this.parentElement.getAttribute('list_id');
-			console.log(elementID);
+			const ownerList = VISUAL_FUNCTIONS.getStorageList('get');
+
+			if (ownerList[elementID].status) {
+				ownerList[elementID].status = false;
+			} else {
+				ownerList[elementID].status = true;
+			};
+
+			VISUAL_FUNCTIONS.getStorageList('set', JSON.stringify(ownerList));
 		});
 		todoDelButton.addEventListener('click', function() {
-			const elementID = this.parentElement.getAttribute('list_id');	
-			console.log(elementID);
+			const elementID = this.parentElement.getAttribute('list_id');
+			const ownerList = VISUAL_FUNCTIONS.getStorageList('get');
+
+			for (let i = Number(elementID) + 1; i < ownerList.length; i++) {
+				const oldElement= document.querySelector(`.todo[list_id="${i}"]`)
+				oldElement.setAttribute('list_id', i - 1);
+			};
+
+			ownerList.splice(elementID, 1);
+			todoWrapper.remove();
+
+			VISUAL_FUNCTIONS.getStorageList('set', JSON.stringify(ownerList));
 		});
 
 		todoWrapper.classList.add('todo');
@@ -56,7 +74,13 @@ const VISUAL_FUNCTIONS = {
 		for (let i = 0; i <= ownerTodoList.length - 1; i++) {
 			this.drawTodo(ownerTodoList[i].text, ownerTodoList[i].status);
 		}
-	}
+	},
+	getStorageList(type = 'get', changeOwnerList = []) {
+		const ownerType = localStorage.getItem('currentOwner');
+
+		if (type === 'get') return JSON.parse(localStorage.getItem(`${ownerType}List`));
+		else if (type === 'set') localStorage.setItem(`${ownerType}List`, changeOwnerList);
+	},
 };
 
 export default VISUAL_FUNCTIONS;
